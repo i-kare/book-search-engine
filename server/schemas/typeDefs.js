fe09b0079cd64c6b11b47e55d872c1c88ef92bae
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-26 20:36:39 -0700

    Fill out type definition for schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,46 +1,47 @@
 //TODO:  Define the necessary Query and Mutation types:
 
-// Query type:
+const { gql } = require('apollo-server-express');
 
-// me: Which returns a User type.
-// Mutation type:
+const typeDefs = gql`
+  type User {
+    _id: ID!,
+    username: String!,
+    email: String,
+    bookCount: Int,
+    savedBooks: [Book]
+  }
 
-// login: Accepts an email and password as parameters; returns an Auth type.
+  type Book {
+    bookId: ID!,
+    authors: [String],
+    description: String,
+    title: String!,
+    image: String,
+    link: String
+  }
 
-// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+  # Set up an Auth type to handle returning data from a user's login or signup
+  type Auth {
+    token: ID!
+    User: User
+  }
 
-// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+  type Query {
+    me: User 
+    # me: Which returns a User type.
+  }
 
-// removeBook: Accepts a book's bookId as a parameter; returns a User type.
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    # Accepts a username, email, and password as parameters; returns an Auth type.
+    login(email: String!, password: String!): Auth
+    # Accepts an email and password as parameters; returns an Auth type.
+    saveBook(bookData: BookInput!): User
+    # Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. 
+    # (Look into creating what's known as an input type to handle all of these parameters!)
+    removeBook(bookId: ID!): User
+    # Accepts a book's bookId as a parameter; returns a User type.
+  }
+`;
 
-// User type:
-
-// _id
-
-// username
-
-// email
-
-// bookCount
-
-// savedBooks (This will be an array of the Book type.)
-
-// Book type:
-
-// bookId (Not the _id, but the book's id value returned from Google's Book API.)
-
-// authors (An array of strings, as there may be more than one author.)
-
-// description
-
-// title
-
-// image
-
-// link
-
-// Auth type:
-
-// token
-
-// user (References the User type.)
+module.exports = typeDefs;
