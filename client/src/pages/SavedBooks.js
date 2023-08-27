62bc76bdf919c41fe10a95979f77f752f7ab79de
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-26 23:07:09 -0700

    forgot to declare it as const

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -18,7 +18,7 @@ const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-  userData = data?.me || {};
+  const userData = data?.me || {};
   
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
