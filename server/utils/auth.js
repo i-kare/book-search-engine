dfe0042299a22446065fb780fd1b498ca92a7b33
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-26 23:41:52 -0700

    fix middleware apollo setup

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({req, res, next}) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -16,8 +16,8 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+      return req;
+    } // if 'token' is not present, return request object as is
 
     // verify token and get user data out of it
     try {
@@ -28,8 +28,8 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return updated req object
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
