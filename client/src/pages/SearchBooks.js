aacbe9f78a38747bfd3619a309befa3a803ef6c4
Karelyn Rojas <knrojas@ucdavis.edu>
2023-08-26 22:23:10 -0700

    Forgot to send variables in save book mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -75,7 +75,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { bookData: bookToSave },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
